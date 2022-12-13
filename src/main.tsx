f098c2fa44eea3eb47d945df08e78f41235d9caf
Luca La Tegola <luca.lategola@gmail.com>
2022-12-13 18:29:14 +0100

    add React Query Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import './index.css'
 import LoaderSpinner from './components/LoaderSpinner'
 import router from './router'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} fallbackElement={<LoaderSpinner />} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} fallbackElement={<LoaderSpinner />} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
