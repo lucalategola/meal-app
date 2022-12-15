ce9f929c196d97444aff326d8648a5778cf0f6b8
Luca La Tegola <luca.lategola@gmail.com>
2022-12-15 21:21:27 +0100

    add react query dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import './index.css'
 import LoaderSpinner from './components/LoaderSpinner'
 import router from './router'
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} fallbackElement={<LoaderSpinner />} />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 )
