ae7028f76ff4072c137f8bde9e814e65c99fb7e8
Luca La Tegola <luca.lategola@gmail.com>
2022-12-15 21:11:06 +0100

    add router provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { RouterProvider } from 'react-router-dom'
 import './index.css'
+import LoaderSpinner from './components/LoaderSpinner'
+import router from './router'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} fallbackElement={<LoaderSpinner />} />
+  </React.StrictMode>
 )
