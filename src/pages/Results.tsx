ee3113d4e8bff653680420d58286cb0014f2686a
Luca La Tegola <luca.lategola@gmail.com>
2022-12-21 18:30:40 +0100

    add layout to results page

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -10,18 +10,28 @@ const Detail = () => {
   if (error) return <p>An error has occurred: ${error.message}</p>
 
   return (
-    <div className="meals">
-      {data?.map((meal) => {
-        return (
-          <Link key={meal.idMeal} to={`/meal/${meal.idMeal}`}>
-            <div className="meal">
-              <h3>{meal.strMeal}</h3>
-              <img src={meal.strMealThumb} alt={meal.strMeal} width="300" />
-            </div>
-          </Link>
-        )
-      })}
-    </div>
+    <section className="categories">
+      <h2 className="section-title">{category}</h2>
+      <div className="slider">
+        {data?.map((meal) => {
+          return (
+            <article key={meal.idMeal} className="category">
+              <h3 className="category-title">
+                <Link to={`/meal/${meal.idMeal}`}>{meal.strMeal}</Link>
+              </h3>
+              <div className="full-img">
+                <Link to={`/meal/${meal.idMeal}`}>
+                  <img src={meal.strMealThumb} alt="{category.strCategory}" />
+                </Link>
+              </div>
+              <Link to={`/meal/${meal.idMeal}`} className="category-link">
+                Open
+              </Link>
+            </article>
+          )
+        })}
+      </div>
+    </section>
   )
 }
 
