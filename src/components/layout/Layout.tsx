87823433d08c6690ef4a36e004e2a0a0c3a00dee
Luca La Tegola <luca.lategola@gmail.com>
2022-12-21 15:44:22 +0100

    add html layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,12 +2,41 @@ import { Link, Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <Link to="/">Home</Link>
-      <Link to="/about">About Us</Link>
-      <Outlet />
-    </div>
+    <>
+      <header className="nav-header">
+        <div className="wrapper">
+          <nav className="nav">
+            <h1>Meal App</h1>
+            <ul className="nav-items">
+              <li className="nav-item">
+                <Link to="/">home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/about">About</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/about">search</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <div className="wrapper">
+        <main>
+          <Outlet />
+        </main>
+      </div>
+      <footer className="footer">
+        <div className="wrapper">
+          <p>
+            Design & Develop by{' '}
+            <a href="" className="footer-link">
+              Luca La Tegola
+            </a>
+          </p>
+        </div>
+      </footer>
+    </>
   )
 }
 
