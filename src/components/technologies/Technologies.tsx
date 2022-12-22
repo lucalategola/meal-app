0b08ef4007c9a523edf8af6293378c7c72efd4d1
Luca La Tegola <lucalategola@users.noreply.github.com>
2022-12-22 18:58:18 +0100

    import logo from assets folder (#13)

--- src/components/technologies/Technologies.tsx
+++ src/components/technologies/Technologies.tsx
@@ -1,36 +1,46 @@
 import styles from './Technologies.module.css'
+import reactLogo from '../../assets/react.svg'
+import typescriptLogo from '../../assets/typescript.svg'
+import reactRouterLogo from '../../assets/react-router.svg'
+import reactQueryLogo from '../../assets/react-query.svg'
 
 const Technologies = () => {
   return (
     <section className={styles.technologies}>
       <article className={styles.technology}>
         <div className={styles['technology-img']}>
-          <a href="" title="Open React Site">
-            <img src="/public/assets/react.svg" alt="react logo" />
+          <a href="https://reactjs.org/" title="Open React Site">
+            <img src={reactLogo} alt="React logo" />
           </a>
         </div>
       </article>
       <article className={styles.technology}>
         <div className={styles['technology-img']}>
-          <a href="" title="Open Typescript Site">
-            <img src="/public/assets/typescript.svg" alt="typescript logo" />
+          <a
+            href="https://www.typescriptlang.org/"
+            title="Open Typescript Site"
+          >
+            <img src={typescriptLogo} alt="Typescript logo" />
           </a>
         </div>
       </article>
       <article className={styles.technology}>
         <div className={styles['technology-img']}>
-          <a href="" title="Open React Router Site">
-            <img
-              src="/public/assets/react-router.svg"
-              alt="react router logo"
-            />
+          <a
+            href="https://reactrouter.com/en/main"
+            title="Open React Router Site"
+          >
+            <img src={reactRouterLogo} alt="React Router logo" />
           </a>
         </div>
       </article>
       <article className={styles.technology}>
         <div className={styles['technology-img']}>
-          <a href="" title="Open React Query Site">
-            <img src="/public/assets/react-query.svg" alt="react qquery logo" />
+          <a
+            href="https://react-query-v3.tanstack.com/"
+            title="Open React Query Site"
+          >
+            <img src={reactQueryLogo} alt="React Query logo" />
           </a>
         </div>
       </article>
